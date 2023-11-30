2fafb917d544b35da34d8b1f6ad191e6f06d0634
JordiMartinez <jordiupc87@gmail.com>
2023-11-30 06:38:58 +0100

    deploy to Sepolia testNetwork

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,11 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+const fs = require("fs");
+let mnemonic = fs.readFileSync(".secret").toString().trim();
+let infuraProjectId = fs.readFileSync(".infura").toString().trim();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  networks{
-    sepolia:{
-      url:""
-    }
-  }
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/" + "23407298307847fe9575a7bc98a97aaa", // this could be infuraProjectId
+      accounts: {
+        mnemonic,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 0,
+      },
+    },
+  },
   solidity: "0.8.20",
 };
