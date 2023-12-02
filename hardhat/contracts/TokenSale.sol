bcb45715c470946ee5d67251870cb4dcc4c95983
JordiMartinez <jordiupc87@gmail.com>
2023-12-02 10:57:18 +0100

    Token Sale contract created

---
+++ hardhat/contracts/TokenSale.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+abstract contract ERC20 {
+    function transfer(address _from,address _to,uint256 _value) public virtual returns (bool success);
+    function decimals() public virtual view returns(uint8);
+}
+
+contract TokenSale {
+    uint tokenPriceInWei = 1 ether;
+    address public tokenOwner;
+
+    ERC20 token;
+
+    constructor(address _token) {
+        token = ERC20(_token);
+    }
+
+    function purchase() public payable {
+        require(msg.value >= tokenPriceInWei, "Not enough money sent");
+        uint tokensToTransfer = msg.value / tokenPriceInWei;
+        uint remainder = msg.value - tokensToTransfer * tokenPriceInWei;
+        token.transfer(tokenOwner,msg.sender, tokensToTransfer * 10 ** token.decimals());
+        payable(msg.sender).transfer(remainder); //send the rest back
+
+    }
+}
