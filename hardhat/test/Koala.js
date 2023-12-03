3d7133eead3c81ab74a0465a0ad9fed880c48b01
JordiMartinez <jordiupc87@gmail.com>
2023-12-03 09:57:41 +0100

    Test added - Transfer token failed

--- hardhat/test/Koala.js
+++ hardhat/test/Koala.js
@@ -9,4 +9,19 @@ describe("Koala", function () {
     await KoalaInstance.safeMint(otherAccount.address);
     expect(await KoalaInstance.ownerOf(0)).to.equal(otherAccount.address);
   });
+
+  it("Fails to transfer tokens from the wrong address", async () => {
+    const Koala = await hre.ethers.getContractFactory("Koala");
+    const KoalaInstance = await Koala.deploy();
+    const [owner, otherAccount, notTheNFTOwner] = await ethers.getSigners();
+    await KoalaInstance.safeMint(otherAccount.address);
+    expect(await KoalaInstance.ownerOf(0)).to.equal(otherAccount.address);
+    await expect(
+      KoalaInstance.connect(notTheNFTOwner).transferFrom(
+        otherAccount.address,
+        notTheNFTOwner.address,
+        0
+      )
+    ).to.be.revertedWith("");
+  });
 });
